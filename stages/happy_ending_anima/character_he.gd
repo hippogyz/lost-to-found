d6c1edb8ee6d39512deac06994d174169f32bd37
hippogyz <hippogyz@hotmail.com>
2021-01-31 01:57:08 +0800

    -9

--- stages/happy_ending_anima/character_he.gd
+++ stages/happy_ending_anima/character_he.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 
 var _velocity : Vector2 = Vector2(200.0, 0.0)
+var dir : float = 0.0
 var controlable : bool = true
 var current_anima : String
+var playing_final : bool = false
 
 func _ready() -> void:
 	current_anima = "idle"
@@ -11,8 +13,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if controlable:
-		var dir = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-		move_and_slide(dir*_velocity)
+		dir = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 		
 		if dir > 0.0:
 			play_anima("normal_walk")
@@ -22,8 +23,32 @@ func _physics_process(delta: float) -> void:
 			get_node("Sprite").scale.x = -1
 		else:
 			play_anima("idle")
+	
+	move_and_slide(dir*_velocity)
 
 func play_anima(anima_name : String) -> void:
 	if current_anima != anima_name:
 		get_node("AnimationTree").get("parameters/playback").travel(anima_name)
 		current_anima = anima_name
+		dir = 0
+
+
+
+func _final_anima_trigger(body: Node) -> void:
+	controlable = false
+	play_anima("sigh")
+
+func anima_sigh_to_walk() -> void :
+	play_anima("normal_hum")
+	dir = 0.5
+
+func _anima_hum(body: Node) -> void:
+	play_anima("hum")
+	dir = 0.75
+
+func _anima_friend(body: Node) -> void:
+	play_anima("friend")
+	dir = 0.0
+
+func _anima_final_of_final() -> void:
+	play_anima("hum")
