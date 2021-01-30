f4dd66d3fa150473a9d41d5bcd4ab417b10ab040
Jueast <jueastwang@outlook.com>
2021-01-30 12:55:00 +0800

    add game over

--- assets/tiles/spikes.gd
+++ assets/tiles/spikes.gd
@@ -1,22 +1,17 @@
 extends Node2D
+signal player_on_spikes()
 
-signal count_down_finish()
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+onready var area = $Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	$VC/CC2/Label2.text = String(int($Timer.time_left))
-	pass
+#func _process(delta):
+#	pass
 
 
-func _on_Timer_timeout():
-	emit_signal("count_down_finish")
+func _on_Area2D_body_entered(body):
+	emit_signal("player_on_spikes")
 	pass # Replace with function body.
