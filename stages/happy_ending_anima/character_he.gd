82c16cdf6a225c9dd1115c6b144cc441509d1990
hippogyz <hippogyz@hotmail.com>
2021-01-31 14:36:31 +0800

    he

--- stages/happy_ending_anima/character_he.gd
+++ stages/happy_ending_anima/character_he.gd
@@ -1,55 +1,49 @@
 extends KinematicBody2D
 
+signal begin_to_walk(is_stop)
+
 var _velocity : Vector2 = Vector2(200.0, 0.0)
 var dir : float = 0.0
-var controlable : bool = true
 var current_anima : String
 var playing_final : bool = false
 
 func _ready() -> void:
-	current_anima = "idle"
-	controlable = false
+	current_anima = ""
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	if controlable:
-		dir = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-		
-		if dir > 0.0:
-			play_anima("normal_walk")
-			get_node("Sprite").scale.x = 1
-		elif dir < 0.0 :
-			play_anima("normal_walk")
-			get_node("Sprite").scale.x = -1
-		else:
-			play_anima("idle")
-	
-	move_and_slide(dir*_velocity)
 
 func play_anima(anima_name : String) -> void:
 	if current_anima != anima_name:
-		get_node("AnimationTree").get("parameters/playback").travel(anima_name)
+		get_node("AnimationPlayer").play(anima_name)
 		current_anima = anima_name
 		dir = 0
 
+func _ini_texture() -> void:
+	$Sprite.visible = false
+	$Sprite2.visible = true
+	play_anima("fall")
 
-
-func _final_anima_trigger(body: Node) -> void:
-	controlable = false
+func _onground_texture() -> void:
+	$Sprite.visible = true
+	$Sprite.frame = 5
+	$Sprite/Sprite2.frame = 2
+	$Sprite2.visible = false
 	play_anima("sigh")
 
+
 func anima_sigh_to_walk() -> void :
-	play_anima("normal_hum")
+	play_anima("normal_walk")
+	emit_signal("begin_to_walk", false)
 	dir = 0.5
 
 func _anima_hum(body: Node) -> void:
 	play_anima("hum")
 	dir = 0.75
 
-func _anima_friend(body: Node) -> void:
-	play_anima("friend")
-	dir = 0.0
+func _anima_friend() -> void:
+	var friends = $Sprite.get_children()
+	for friend in friends:
+		if friend.get_node("AppearAnimation") != null:
+			friend.get_node("AppearAnimation").play("appear")
 
 func _anima_final_of_final() -> void:
 	play_anima("hum")
--- stages/happy_ending_anima/happy ending.gd
+++ stages/happy_ending_anima/happy ending.gd
@@ -1,20 +1,38 @@
 extends Node2D
 
 export var pre_time_max : float = 3.0
-var pre_time
+export var first_walk_time : float = 6.0
+var first_played = false
+export var second_walk_time : float = 5.0
+var second_played = false
+var current_time
 onready var pre_curtain = get_node("ColorRect")
 var pre_anima_playing
 
 func _ready():
 	pre_curtain.color.a = 1
-	pre_time = pre_time_max
+	current_time = 0.0
 	pre_anima_playing = true
+	
+	first_walk_time = pre_time_max + first_walk_time
+	second_walk_time = first_walk_time + second_walk_time
 
 func _process(delta: float) -> void:
-	if pre_anima_playing:
-		if pre_time > 0:
-			pre_time -= delta
-			pre_curtain.color.a = max( 0.0, pre_curtain.color.a - 1.0 / pre_time_max * delta)
-		else:
-			pre_anima_playing = false
-			get_node("character_he").controlable = true
+	_play_animation(current_time)
+	current_time += delta
+
+func _play_animation(time : float) -> void:
+	if time < pre_time_max:
+		pass
+	elif time > first_walk_time && !first_played:
+		$character_he.play_anima("hum")
+		first_played = true
+	elif time > second_walk_time && !second_played:
+		$character_he._anima_friend()
+		second_played = true
+	
+
+func _set_background_move(is_stop : bool) -> void:
+	var bg_list = $ParallaxBackground/ParallaxLayer.get_children()
+	for bg in bg_list:
+		bg.is_stop = is_stop
